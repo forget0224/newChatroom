ff0bc7d8974575675badbed020db618e2ce12523
lulu <ybkjb18@gmail.com>
2023-07-21 18:50:58 +0800

    fix RWD

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -6,14 +6,13 @@ import dayjs from "dayjs";
 
 const message = ref("");
 const username = ref("");
-const isLockName = ref(false)
+const isLockName = ref(false);
 
 function isSelf(name) {
   return username.value === name
 }
 
 const time = dayjs().format("H:mm A");
-
 
 const addMessage = () => {
   if (!message.value.trim()) {
@@ -29,15 +28,17 @@ const addMessage = () => {
   });
   message.value = "";
 };
+
+
 </script>
 
 <template >
   <div class="bg-slate-50 h-screen w-screen">
-    <div class="container bg-slate-50 h-screen w-full m-auto flex">
-      <aside class="h-screen w-96 bg-slate-50">
+    <div class="container bg-slate-50 h-screen w-full m-auto flex sm:flex-row flex-col">
+      <aside class="sm:h-screen h-[100px] w-96 bg-slate-50">
         <div class="m-auto w-80">
-          <h1 class="text-slate-500 text-3xl mt-40">Live Chat Room</h1>
-          <div class="mt-20 text-slate-500 flex">
+          <h1 class="text-slate-500 text-3xl sm:mt-40 pt-4">Live Chat Room</h1>
+          <div class="sm:mt-20 pt-4 text-slate-500 flex">
             <label for="username" class="">username</label>
             <input v-model="username" @keyup.enter="isLockName = true" :disabled="isLockName" type="text" id="username"
               placeholder="name type here" class="rounded pl-2 ml-3 w-40" />
@@ -48,13 +49,13 @@ const addMessage = () => {
           </div>
         </div>
       </aside>
-      <main class="h-screen grow relative">
-        <div class="message-box  h-[85%] overflow-y-scroll scrollbar scrollbar-thin scrollbar-thumb-slate-600">
+      <main class="sm:h-screen grow relative">
+        <div class="message-box  h-[80%] overflow-y-scroll scrollbar scrollbar-thin scrollbar-thumb-slate-600 sm:mt-20 mt-6 bg-[#cbd5e1]">
           <template v-for="item in chatroomRefList" :key="item.id">
             <div class="message-other  h-36 flex flex-wrap relative">
-              <div class="flex h-20 max-w-[80%] min-w-[60%] m-auto items-center   absolute  shadow-lg"
+              <div class="flex h-20 max-w-[80%] min-w-[60%] m-auto items-center   absolute  shadow-lg mt-4"
                 :class="[{ ' left-8  rounded-tl-lg rounded-tr-lg rounded-br-lg bg-white': !isSelf(item.username) }, { ' right-8 rounded-tl-lg rounded-tr-lg rounded-bl-lg flex-row-reverse bg-slate-200': isSelf(item.username) }]">
-                <div class="rounded-full bg-slate-300 w-16 h-16 self-auto flex justify-center "
+                <div class="rounded-full bg-slate-300 w-16 sm:h-16 h-12 self-auto flex justify-center "
                   :class="[{ 'ml-3': !isSelf(item.username) }, { 'mr-3': isSelf(item.username) }]">
                   <span class="text-4xl justify-center  flex items-center font-mono">{{ item.username.slice(0, 1)
                   }}</span>
@@ -73,11 +74,11 @@ const addMessage = () => {
           </template>
         </div>
 
-        <div class="type-box absolute bottom-8 w-full h-16">
-          <div class="flex h-full w-[92.6667%] m-auto">
-            <div class="rounded-full bg-slate-500 w-16 h-16 self-auto flex justify-center">
+        <div class="type-box absolute  w-full h-16">
+          <div class="flex h-[100px] w-[92.6667%] m-auto items-center">
+            <div class="rounded-full bg-slate-500 w-16 sm:h-16 h-14 self-auto flex justify-center">
               <span class="text-4xl justify-center  flex items-center font-mono">
-                {{ username.slice(0, 1) }}</span>
+                {{ isLockName ? username.slice(0, 1) : '' }}</span>
             </div>
             <input type="text" class="h-16 w-11/12 bg-transparent pl-3 focus:border-transparent ml-3 outline-0"
               placeholder="type something.." v-model="message" @keyup.enter="addMessage" :disabled="!username" />
